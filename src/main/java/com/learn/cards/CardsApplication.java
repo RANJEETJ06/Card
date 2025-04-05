8427910dd238f27a90ad9564aef69ea8d146a707
Ranjeet Kumar Jena <121615244+RANJEETJ06@users.noreply.github.com>
2025-04-05 22:36:06 +0530

    Initial commit

---
+++ src/main/java/com/learn/cards/CardsApplication.java
@@ -0,0 +1,34 @@
+package com.learn.cards;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
+@EnableJpaRepositories("com.eazybytes.cards.repository")
+@EntityScan("com.eazybytes.cards.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info =@Info(
+				title = "Card MicroService",
+				description = "Card Microservice using SpringBoot",
+				version = "v1",
+				contact = @Contact(
+						name = "Ranjeet Jena",
+						url = "https://github.com/RANJEETJ06",
+						email = "ranjeetjena06@gmail.com"
+				)
+		)
+)
+public class CardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApplication.class, args);
+	}
+}
