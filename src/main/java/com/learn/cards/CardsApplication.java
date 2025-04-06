2e2b4a0141bccae6e03f2ec2a3394171b953dc2a
Ranjeet Kumar Jena <121615244+RANJEETJ06@users.noreply.github.com>
2025-04-06 13:38:24 +0530

    Update Java version to 17 and adjust package references in CardsApplication

--- src/main/java/com/learn/cards/CardsApplication.java
+++ src/main/java/com/learn/cards/CardsApplication.java
@@ -1,18 +1,16 @@
 package com.learn.cards;
 
-import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-/*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
-@EnableJpaRepositories("com.eazybytes.cards.repository")
-@EntityScan("com.eazybytes.cards.model")*/
+/*@ComponentScans({ @ComponentScan("com.ranjeet.cards.controller") })
+@EnableJpaRepositories("com.ranjeet.cards.repository")
+@EntityScan("com.ranjeet.cards.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
 		info =@Info(
